2448d358c35da2a572173803030f5f985676342a
DiminutiveDev <43453576+Diminutivecoin@users.noreply.github.com>
2020-01-20 12:51:07 +0200

    V1003 Protocol update to reject older versions

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70000;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60001;
+static const int PROTOCOL_VERSION = 60004;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 200;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60000;
+static const int MIN_PEER_PROTO_VERSION = 60003;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
